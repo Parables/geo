ec3ccae8fac7e078b5c7bd41b3b996f96704ae6e
parables <38154990+Parables@users.noreply.github.com>
2023-05-19 04:26:03 +0000

    feature(Actions): build a nested set model by reading the hierarchy file

---
+++ src/Actions/BuildNestedSetAction.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Parables\Geo\Actions;
+
+use Illuminate\Support\LazyCollection;
+use Parables\Geo\Actions\Concerns\HasToastable;
+use Parables\Geo\Models\GeoName;
+
+class BuildNestedSetAction
+{
+    use HasToastable;
+
+    /**
+     * @param LazyCollection<array-key,mixed> $hierarchyCollection
+     */
+    public function execute(LazyCollection $hierarchyCollection, int $chunkSize = 1000): void
+    {
+        ini_set('memory_limit', -1);
+        ini_set('auto_detect_line_endings', TRUE);
+
+        $hierarchyCollection->chunk($chunkSize)->each(function (LazyCollection $lazyCollection) {
+            $lazyCollection->each(function (string $line) {
+
+                [$parentId, $childId] = array_map('trim', explode("\t", $line));
+                $parent = GeoName::where('parent_id', $parentId)->first();
+                $child = GeoName::where('id', $childId)->first();
+                if ($parent && $child) {
+                    $parent->appendNode($child);
+                }
+                // TODO: Handle cases for earth and continents as roots
+                // if ($parent->isEarth()) {
+                // $parent->makeRoot()->save();
+                // }
+            });
+        });
+    }
+}
