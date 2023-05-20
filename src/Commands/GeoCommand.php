160f38fcd51acaf78796d2ed013242b96d5228c9
parables <38154990+Parables@users.noreply.github.com>
2023-05-20 07:41:26 +0000

    feature(Commands): download, unzip, read, transform and load countries into DB

--- src/Commands/GeoCommand.php
+++ src/Commands/GeoCommand.php
@@ -4,13 +4,17 @@ namespace Parables\Geo\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
+use Illuminate\Support\LazyCollection;
 use Parables\Geo\Actions\Concerns\Toaster;
 use Parables\Geo\Actions\Contracts\Toastable;
 use Parables\Geo\Actions\DownloadAction;
 use Parables\Geo\Actions\FilterFileNames;
 use Parables\Geo\Actions\GetDownloadLinksAction;
 use Parables\Geo\Actions\ListCountries;
+use Parables\Geo\Actions\LoadGeonamesAction;
+use Parables\Geo\Actions\ReadFileAction;
+use Parables\Geo\Actions\TransformGeonamesAction;
+use Parables\Geo\Actions\UnzipAction;
 
 class GeoCommand extends Command implements Toastable
 {
@@ -18,52 +22,175 @@ class GeoCommand extends Command implements Toastable
 
     public $signature = 'geo:install';
 
-    public $description = 'Populates your database your data from geonames.org';
+    public $description = 'Populates your database with data from geonames.org';
 
     const BASE_URL = "http://download.geonames.org/export/dump/";
+    const GEONAMES_ORG = "[Geonames.org](" . self::BASE_URL . ')';
 
     public function handle(): int
     {
+        $this->introduceCommand();
 
-        $countries = (new ListCountries)
-            ->execute(
-                (new FilterFileNames)
-                    ->execute(
-                        (new GetDownloadLinksAction)
-                            ->execute('http://download.geonames.org/export/dump/')
-                    )
-            );
+        $countries = $this->fetchCountries();
 
         $auxilaryFileNames = ['no-country.zip', 'hierarchy.zip', 'alternateNamesV2.zip', 'countryInfo.txt',];
 
-        $choice = $this->choice('Select the countries would you like to populate your database with: ', ['All countries', 'List available countries'], 0);
+        $fileNames = $this->askToSelectCountries(countries: $countries);
 
-        if ($choice === 0) {
-            $fileNames = [...$auxilaryFileNames, ...array_map(fn ($country) => $country . '.zip', $countries)];
+        $this->downloadFiles(fileNames: $fileNames);
 
-            $this->comment('Download List: ' . json_encode($fileNames, JSON_PRETTY_PRINT));
-        } else {
-            $choice = $this->choice('Available countries:', [...$countries, 'ALL' => 'Select all countries'], 'ALL', multiple: true);
+        $fileNames =   $this->unzipFiles(fileNames: $fileNames);
+
+        $fileCollection = $this->readFiles(fileNames: $fileNames);
+
+        $this->processCountryFiles(filesCollection: $fileCollection);
+
+        $this->comment('All done... Enjoy :-)');
+        return self::SUCCESS;
+    }
+
+    private function introduceCommand(): void
+    {
+        $this->confirm(
+            'This command will download a bunch of files from ' .
+                self::GEONAMES_ORG .
+                ', unzip the files, parse the files and populate your database. ' .
+                'Ensure you have a stable internet connection to download roughly 3GB of data. ' .
+                'Are you sure you want to proceed? ',
+            default: true,
+        );
+    }
+
+    public function fetchCountries(): array
+    {
+        $this->info('Fetching updates from: ' . self::GEONAMES_ORG . ' ... Please wait...');
+
+        $countries = (new ListCountries)->execute(
+            fileNames: (new FilterFileNames)->execute(
+                fileNames: (new GetDownloadLinksAction)->execute(url: 'http://download.geonames.org/export/dump/'),
+                regex: FilterFileNames::COUNTRY_FILENAME_REGEX,
+            ),
+        );
+
+        $this->info('Update complete... Fetched ' . count($countries) . ' countries from: ' . self::GEONAMES_ORG);
+
+        return $countries;
+    }
+
+    private function askToSelectCountries(array $countries): array
+    {
+        $choice = $this->choice(
+            question: 'How would you like to proceed?: ',
+            choices: [
+                'Download only selected countries',
+                'Download all countries',
+            ],
+            default: '0',
+        );
+
+        if ($choice === '0') {
+            $choice = $this->choice(
+                question: 'Enter a comma-separated list of countries to be downloaded: ',
+                choices: [
+                    ...$countries,
+                    'ALL' => 'Download all countries'
+                ],
+                default: 'ALL',
+                multiple: true,
+            );
+
             $choice = Arr::wrap($choice);
-
             $this->comment('Selected countries: ' . json_encode($choice, JSON_PRETTY_PRINT));
+
+            if (in_array('ALL', $choice)) {
+                return $this->appendFileExtension(countryCodes: array_keys($countries));
+            }
+            return $this->appendFileExtension($choice);
         }
 
-        // $selectedCountries = array_map(fn ($fileName) => self::BASE_URL . $fileName, $selectedCountries);
+        return $this->appendFileExtension(countryCodes: array_keys($countries));
+    }
 
-        $overwrite = $this->choice('What should be done if the file has already been downloaded?', ['Skip', 'Overwrite']);
+    private function appendFileExtension(array $countryCodes): array
+    {
+        return array_map(fn ($code) => $code . '.zip', $countryCodes);
+    }
 
-        $this->info('Your choice was: ' . $overwrite);
+    public function downloadFiles(array $fileNames)
+    {
+        $overwrite = $this->choice(
+            question: 'Download is about to begin... What should be done if the file has already been downloaded?',
+            choices: [
+                'Skip',
+                'Overwrite',
+            ],
+            default: '0',
+        );
 
-        // $downloadAction = (new DownloadAction)->toastable($this)->execute([$selectedCountries], $overwrite ?? false);
+        $overwrite = boolval($overwrite);
+
+        $this->comment('Previously downloaded files will be' . $overwrite ? 'Overwritten' : 'Skipped');
+
+        // TODO: implement a ProgressBar
+        (new DownloadAction)
+            ->toastable($this)
+            ->execute(fileNames: $fileNames, overwrite: $overwrite);
+    }
+
+    public function unzipFiles(array $fileNames)
+    {
+        $this->info('Unzipping compressed files...');
+
+        $unzipAction = (new UnzipAction)->toastable($this);
+        $result = [];
+
+        $this->withProgressBar($fileNames, function (string $fileName) use ($unzipAction) {
+            $this->info('Current file: ' . $fileName);
+            $fileName[] =  $unzipAction->execute(fileName: $fileName, overwrite: true);
+        });
+
+        return $result;
+    }
+
+    public function readFiles(array $fileNames)
+    {
+        $this->info('Reading contents of files...');
+
+        $readFileAction =   (new ReadFileAction)->toastable($this);
+
+        return LazyCollection::make(function () use ($fileNames, $readFileAction) {
+            foreach ($fileNames as $fileName) {
+                $this->info('Current file: ' . $fileName);
+                yield $readFileAction->execute($fileName);
+            }
+        });
+    }
 
 
-        // foreach ($selectedCountries as $key => $value) {
+    public function processCountryFiles(LazyCollection $filesCollection)
+    {
+        $this->info('Processing file contents... This might take a while so please be patient...');
 
-        // }
+        $transformGeonamesAction = (new TransformGeonamesAction)->toastable($this);
+        $loadGeonamesAction = (new LoadGeonamesAction)->toastable($this);
 
-        $this->comment('All done');
+        $filesCollection->chunk(50)->each(function (LazyCollection $filesCollection) use ($transformGeonamesAction, $loadGeonamesAction) {
+            $this->info('Processing next chunk: ...');
 
-        return self::SUCCESS;
+            $filesCollection->each(function (LazyCollection $fileContentCollection) use ($transformGeonamesAction, $loadGeonamesAction) {
+                $this->info('Transforming the file contents into a collection of Geonames...');
+                $geonamesCollection = $transformGeonamesAction->execute(
+                    geonamesCollection: $fileContentCollection,
+                    toPayload: true,
+                    idAsindex: true
+                );
+                $this->info('Inserting the collection into the database... This might take a while... Hang in there...');
+                // $loadGeonamesAction->execute(
+                //     geonamesCollection: $geonamesCollection,
+                //     chunkSize: 1000,
+                //     truncateBeforeInsert: false
+                // );
+            });
+        });
     }
 }
